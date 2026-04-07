257410912b8f60e47e7043e81ade60f96f83e526
Alex K <nixomsk@gmail.com>
2026-04-07 16:48:31 +0600

    Deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/krugomer/' : '/',
   plugins: [
     TanStackRouterVite({
       routesDirectory: './src/routes',
